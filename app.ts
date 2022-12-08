d5d3a1ff329185ba7d5a37fd976fa304640dbfb0
xchezzzx <50829021+xchezzzx@users.noreply.github.com>
2022-12-08 11:30:14 +0200

    GET req from html

--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Response, Request } from "express";
 import { studentRouter } from "./application/student/routes";
+import cors from "cors";
 
 const app: Application = express();
 
 app.use(express.json());
 
+app.use(cors());
+app.use(myLogger);
 app.use("/api/students", studentRouter);
+
+function myLogger(req: Request, res: Response, next: NextFunction) {
+    console.log("My logger appears");
+    console.log(req.url);
+    next();
+}
 
 export { app };
